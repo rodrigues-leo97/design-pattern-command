51f8be876f2deca17d25cffa9c69acb5997adcaf
rodrigues-leo97 <990562@adm.bancoamazonia.sa>
2024-02-05 11:56:23 -0300

    feat: padrozinação command implementada e novo uso SWITCH CASE

---
+++ src/main/java/br/com/alura/designPatterns/command/Command.java
@@ -0,0 +1,5 @@
+package br.com.alura.designPatterns.command;
+
+public interface Command {
+    void execute();
+}
--- src/main/java/br/com/alura/designPatterns/command/CommandExecutor.java
+++ src/main/java/br/com/alura/designPatterns/command/CommandExecutor.java
@@ -1,4 +1,4 @@
-package br.com.alura;
+package br.com.alura.designPatterns.command;
 
 public class CommandExecutor {
 
